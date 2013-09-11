53c30daeab265d09a56a8e1f7416f909f08d3de1
Stephan Krauss <info@stephankrauss.de>
2013-09-11 20:16:00 +0200

    Skripte zu den Videos der Firma Video2Brain

---
+++ skripte/Kapitel 03 - Grid/03-12 Editieren/vorher/data.js
@@ -0,0 +1,56 @@
+"use strict";
+(function () {
+    window.v2bIgnite = window.vb2Ignite || {};
+
+    v2bIgnite.data = [
+                { id: 1, title: 'ASP.NET MVC', subtitle: 'Ein einfacher Einstieg', Category: 'Programmierung',
+                    Trainer: 'Christian Wenz', Price: 19.95
+                },
+                { id: 2, title: 'JavaScript - Crashkurs für .NET-Entwickler', subtitle: 'Der praktische Einstieg', Category: 'Programmierung',
+                    Trainer: 'Ralph Steyer', Price: 24.95
+                },
+                { id: 3, title: 'Node.js – Crashkurs', subtitle: 'Serverseitige Anwendungen mit JavaScript', Category: 'Programmierung',
+                    Trainer: 'André Krämer', Price: 29.95
+                },
+                { id: 4, title: 'Windows 8 Apps mit HTML5 und JavaScript – Das große Training', subtitle: 'Anwendungen für Windows RT planen, entwickeln und veröffentlichen',
+                    Category: 'Programmierung', Trainer: 'André Krämer', Price: 39.95
+                },
+                { id: 5, title: 'Google Chart API – Crashkurs', subtitle: 'Ohne Programmierkenntnisse einsteigen',
+                    Category: 'Programmierung', Trainer: 'Thomas Pfeiffer', Price: 29.95
+                },
+                { id: 6, title: 'Windows Store Apps mit XAML und C# – Das große Training', subtitle: 'Anwendungen für Windows RT planen, entwickeln und veröffentlichen',
+                    Category: 'Programmierung', Trainer: 'Gregor Biswanger', Price: 39.95
+                },
+                { id: 7, title: 'Fireworks CS6 – Das große Training', subtitle: 'Grundlagen, Webgrafiken, interaktive Prototypen, moderne Screendesigns, Expertentipps',
+                    Category: 'Bildbearbeitung & Fotografie', Trainer: 'André Reinegger', Price: 39.95
+                },
+                { id: 8, title: 'Photoshop-Praxis für Fotografen: Farbkorrekturen', subtitle: 'Anwendungen für Windows RT planen, entwickeln und veröffentlichen',
+                    Category: 'Bildbearbeitung & Fotographie', Trainer: 'Olaf Giermann', Price: 24.95
+                },
+                { id: 9, title: 'Encore CS6 – Crashkurs', subtitle: 'DVDs, Blu-ray-Discs und Web-DVDs erstellen und veröffentlichen',
+                    Category: 'Audio & Video', Trainer: 'Sven Brencher', Price: 24.95
+                },
+                { id: 10, title: 'Photoshop im Agenturalltag', subtitle: 'Die ersten Schritte für Neueinsteiger und Quereinsteiger',
+                    Category: 'Design', Trainer: 'Ralf Mack'
+                },
+                { id: 11, title: 'Interaktive Dokumente mit InDesign CS5 und CS5.5', subtitle: 'Gestalten Sie interaktive PDFs, Websites und mehr!',
+                    Category: 'Design', Trainer: 'José Ramos, Gerald Singelmann', Price: 29.95
+                },
+                { id: 12, title: 'CSS Layouts', subtitle: 'Flexible Websites einfach erstellt',
+                    Category: 'Web', Trainer: 'Florence Maurice', Price: 24.95
+                },
+                { id: 13, title: 'Meine erste Website', subtitle: 'Erfolgreich planen, bauen, online gehen',
+                    Category: 'Web', Trainer: 'Lydia Schimpf', Price: 24.95
+                },
+                { id: 14, title: 'HTML5 – Crashkurs', subtitle: 'Der schnelle Weg zu modernen Websites',
+                    Category: 'Web', Trainer: 'Christian Wenz', Price: 24.95
+                },
+                { id: 15, title: 'Microsoft WebMatrix', subtitle: 'Dynamische Webanwendungen erstellen',
+                    Category: 'Programmierung', Trainer: 'Christian Wenz', Price: 19.95
+                },
+                { id: 16, title: 'Excel 2013 Profiwissen', subtitle: 'Vom Kenner zum Könner!',
+                    Category: 'Business', Trainer: 'Wolfram E. Mewes', Price: 39.95
+                }
+
+            ];
+})();
